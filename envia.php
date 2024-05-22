009807e76fd43c5c5131d0cae2dec9cd2850cd7f
Victor Xavier <148595747+VaxDeveloper@users.noreply.github.com>
2024-05-22 11:05:45 -0300

    #

--- envia.php
+++ envia.php
@@ -86,7 +86,7 @@ if (!$conexao) {
                     $update_resultado = mysqli_query($conexao, $update_sql);
 
                     if ($update_resultado) {
-                        echo "<div class='fs-6 mb-1'>Link para download do vídeo adicionado à tabela no banco de dados.</div><br>";
+                        echo "<div class='fs-6 mb-5'>Link para download do vídeo adicionado à tabela no banco de dados.</div><br>";
                         echo "<div class='fs-6'>Vídeo salvo com sucesso!</div>";
                     } else {
                         echo "<div class='fs-6'>ERRO AO ATUALIZAR O LINK DE DOWNLOAD NA TABELA: </div>" . mysqli_error($conexao);
