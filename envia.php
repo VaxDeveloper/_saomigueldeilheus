cc4749071eb193664f20465b281bca40789626ff
Victor Xavier <148595747+VaxDeveloper@users.noreply.github.com>
2024-05-22 11:02:58 -0300

    #

--- envia.php
+++ envia.php
@@ -87,7 +87,7 @@ if (!$conexao) {
 
                     if ($update_resultado) {
                         echo "<div class='text-light fs-6'>Link para download do vídeo adicionado à tabela no banco de dados.</div><br>";
-                        echo "<div text-light class='fs-6'>Vídeo salvo com sucesso!</div>";
+                        echo "<div class='text-light fs-6'>Vídeo salvo com sucesso!</div>";
                     } else {
                         echo "<div class='text-light  fs-6'>ERRO AO ATUALIZAR O LINK DE DOWNLOAD NA TABELA: </div>" . mysqli_error($conexao);
                     }
